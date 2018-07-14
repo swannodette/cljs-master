1e2125f9017f17604e00d693eaf92ee1e3b14d51
dnolen <dnolen@cognitect.com>
2018-07-13 22:15:53 -0400

    compiling WIP

--- src/cljs_master/compiling.clj
+++ src/cljs_master/compiling.clj
@@ -1,1 +1,75 @@
-(ns cljs-master.compiling)
+(ns cljs-master.compiling
+  (:require [cljs.env :as env]
+            [cljs.analyzer :as ana]
+            [cljs.compiler :as comp]))
+
+(def aenv (ana/empty-env))
+(def cenv (env/default-compiler-env))
+
+(def a-defn '(defn foo [a b] (+ a b)))
+
+(comment
+
+  (env/with-compiler-env cenv
+    (comp/emit
+      (ana/analyze-form aenv a-defn nil nil)))
+
+  )
+
+;; -----------------------------------------------------------------------------
+;; Exercise 1:
+;;
+;; We would like to compile cenv in a different namespace. What do you think
+;; we have to change? Examine ana/empty-env to get a clue.
+
+;; -----------------------------------------------------------------------------
+;; Exercise 2:
+
+(def a-type '(deftype A []))
+
+(comment
+
+  (env/with-compiler-env cenv
+    (comp/emit
+      (ana/analyze-form aenv a-type nil nil)))
+
+  )
+
+;; What does deftype compile to? Unfortunately we get a warning? What's your
+;; hypothesis about this warning? Have we analyzed core? If not how can
+;; we analyze it before we analyze our form?
+
+;; -----------------------------------------------------------------------------
+;; Exercise 3:
+
+(def a-defn-2 '(defn bar [c d] (foo c d)))
+
+(comment
+
+  (env/with-compiler-env cenv
+    (comp/emit
+      (ana/analyze-form aenv a-defn-2 nil nil)))
+
+  )
+
+;; When we compile bar we see the invoke to foo will be cljs.user.foo.call(...)
+;; instead of a direct call cljs.user.foo(...). Examine the :invoke case in
+;; cljs.compiler, is there something we can do to optimize this call?
+
+;; -----------------------------------------------------------------------------
+;; Exercise 4:
+
+(def a-proto '(defprotocol IFoo (-foo [a b])))
+
+(comment
+
+  (binding [ana/*unchecked-if* false]
+    (env/with-compiler-env cenv
+      (comp/emit
+        (ana/analyze-form aenv a-proto nil nil))))
+
+  )
+
+;; This analysis will fail. How we do we fix it? Once fixed describe the
+;; the output. What do protocol fns do?
+
